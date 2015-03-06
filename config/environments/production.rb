f036511c12eb45d81b33c2e09def638c211d6d2b
Roman Sklenář <mail@romansklenar.cz>
2015-03-06 11:30:15 +0000

    Configure React variants to use in environments

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Use minified version of React which strips out comments and helpful warnings.
+  config.react.variant = :production
 end
